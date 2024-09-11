c714c97a19d10f9d08b831b2f19e1d6d0e04b176
c_dock <153040099+evollivia@users.noreply.github.com>
2024-09-11 13:56:04 +0900

    terraform AWS EC2 instance - ubuntu

---
+++ ec2_instance_ubuntu/variables.tf
@@ -0,0 +1,29 @@
+variable "ami" {
+  description = "ami of instance"
+  type = string
+}
+
+variable "instance_type" {
+  description = "instance type of instance"
+  type = string
+}
+
+variable "key_name" {
+  description = "key name of instance"
+  type = string
+}
+
+variable "instance_name" {
+  description = "instance name for instance"
+  type = string
+}
+
+variable "security_group_id" {
+  description = "id for assign to instance"
+  type = string
+}
+
+variable "user_data" {
+  description = "user script to execute"
+  type = string
+}
