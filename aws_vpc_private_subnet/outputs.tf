1b519aac4e80cfce1b4c224aeff7ea04611da1b7
c_dock <153040099+evollivia@users.noreply.github.com>
2024-09-20 10:49:48 +0900

    수정된 output

--- aws_vpc_private_subnet/outputs.tf
+++ aws_vpc_private_subnet/outputs.tf
@@ -1,9 +1,31 @@
+output "private_subnet_ids" {
+  value = {
+    for key, subnet in aws_subnet.private_subnet: key => subnet.id
+  }
+}
+
+output "private_sg_ids" {
+  value = {
+    for key, sg in aws_security_group.private_sg: key => sg.id
+  }
+}
+
+
+
 # # 프라이빗 서브넷 ID 출력
-# output "private_subnet_id" {
-#   value = aws_subnet.private_subnet1.id
+# output "private_subnet_fastapi_id" {
+#   value = aws_subnet.private_subnet["fastapi"].id
+# }
+#
+# output "private_subnet_mariadb_id" {
+#   value = aws_subnet.private_subnet["mariadb"].id
 # }
 #
 # # 보안 그룹 ID 출력
-# output "private_sg_id" {
-#   value = aws_security_group.private_sg1.id
+# output "private_sg_fastapi_id" {
+#   value = aws_security_group.private_sg["fastapi"].id
+# }
+#
+# output "private_sg_mariadb_id" {
+#   value = aws_security_group.private_sg["mariadb"].id
 # }
